093914b9dca53aeb36e247781b04cb77f7a530cd
Yak! <yak1ex@users.noreply.github.com>
2018-04-11 21:45:04 +0900

    [config_maker] Initial working version.

--- config_maker/config_maker.js
+++ config_maker/config_maker.js
@@ -2,8 +2,11 @@ const Promise = require('bluebird')
 const fs = Promise.promisifyAll(require('fs-extra'))
 const path = require('path')
 const vm = require('vm')
+const readline = require('readline')
 const iconv = require('iconv-lite')
 const Mustache = require('mustache')
+const JsDiff = require('diff')
+const minimatch = require("minimatch")
 
 function make_context () {
   const Counter = class {
@@ -50,34 +53,77 @@ function make_context () {
   return vm.createContext(context_base)
 }
 
+const question = (query, choice) => new Promise((resolve, reject) => {
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+  const cb = (answer) => {
+    if(choice.includes(answer)) {
+      rl.close()
+      resolve(answer)
+    } else {
+      rl.question(query, cb)
+    }
+  }
+  rl.question(query, cb)
+})
+
+const match = (filename, choice, defval) => {
+  if (!choice) return defval
+  const ret = choice.find(e => minimatch(filename, e[0]))
+  return (ret !== undefined) ? ret[1] : defval
+}
+
 const [ input, temp ] = process.argv.slice(2, 4).map(v => path.join(__dirname, v))
 const work = path.join(temp, '#work')
-const context = make_context()
+const origContext = make_context()
 
 ;(async function main() {
 
   await fs.removeAsync(work)
   await fs.mkdirpAsync(work)
   let files = await fs.readdirAsync(input)
+console.log(input)
 
   files.filter(v => v.match(/\.js$/)).map(async filename => {
-// collect
+// collect phase
     const script = await fs.readFileAsync(path.join(input, filename))
+    const context = Object.assign(origContext)
     vm.runInContext(script, context, { filename, displayErrors: true })
     console.log(context)
 
-// create
+// create phase
     const dirname = filename.substring(0, filename.length - 3)
     const outdir = path.join(work, dirname)
     await fs.mkdirAsync(outdir)
     const files = await fs.readdirAsync(path.join(input, dirname))
-    files.map(async filename => {
+    await Promise.all(files.map(async filename => {
+      const encoding = match(filename, context.encoding, 'sjis')
       const buf = await fs.readFileAsync(path.join(input, dirname, filename))
-      // FIXME: encoding must be configurable
-      const encoding = 'sjis'
       fs.writeFileAsync(path.join(outdir, filename), iconv.encode(Mustache.render(iconv.decode(buf, encoding), context), encoding))
-    })
-  })
-})() // invoking main
+    }))
 
 // install phase
+    const filesTemp = await fs.readdirAsync(path.join(work, dirname))
+    filesTemp.map(async filename => {
+      const instdir = match(filename, context.install)
+      if(instdir) {
+        const encoding = match(filename, context.encoding, 'sjis')
+        const curr = path.join(instdir, filename)
+        const temp = path.join(outdir, filename)
+        const currContent = iconv.decode(await fs.readFileAsync(curr), encoding)
+        const tempContent = iconv.decode(await fs.readFileAsync(temp), encoding)
+        console.log(JsDiff.createTwoFilesPatch(curr, temp, currContent, tempContent))
+        if (currContent === tempContent) {
+          console.log('No differences, skip')
+        } else {
+          await question('apply Y/N? ', ['Y','N']).then(answer => {
+            if(answer === 'Y') {
+              fs.copyAsync(temp, curr)
+            }
+          })
+        }
+      }
+    })
+  })
+
+})() // invoking main
+
