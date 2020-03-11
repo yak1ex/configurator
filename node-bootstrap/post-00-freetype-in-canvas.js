f7cc630929ef5efd02f4201a9f19fdb53ec2b324
Yak! <yak1ex@users.noreply.github.com>
2020-03-11 21:09:36 +0900

    [node-bootstrap] Update FreeType DLL anme.

--- node-bootstrap/post-00-freetype-in-canvas.js
+++ node-bootstrap/post-00-freetype-in-canvas.js
@@ -4,6 +4,6 @@ const npm_prefix = exec_get('npm config get prefix').trim()
 const target = path.join(npm_prefix, 'node_modules/canvas/build/Release');
 
 if(!fs.existsSync(path.join(target, 'libfreetype-6.dll'))) {
-  console.log('copy freetype6.dll');
-  copy(path.join(gtk_prefix, 'bin/freetype6.dll'), path.join(target, 'freetype6.dll'));
+  console.log('copy libfreetype-6.dll');
+  copy(path.join(gtk_prefix, 'bin/libfreetype-6.dll'), path.join(target, 'libfreetype-6.dll'));
 }
