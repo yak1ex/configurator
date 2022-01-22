3c7420f24b31529ed0352a748875cdf4d896372b
Yak! <yak1ex@users.noreply.github.com>
2022-01-22 14:18:53 +0900

    [bootstrap] Remove unnecessary blanks.

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -262,7 +262,7 @@ function GetRVerGtk {
     32='http://ftp.gnome.org/pub/GNOME/binaries/win32/gtk+/2.24/gtk+-bundle_2.24.10-20120208_win32.zip';
     64='http://ftp.gnome.org/pub/GNOME/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip'
   }
-  
+
   $results=@{}
   foreach($key in $spec.Keys) {
     $results[$key]=@{url=$gtk[$key]}
