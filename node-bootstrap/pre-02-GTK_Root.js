9c71e3b24b73e0711837f899a7e74e337a552369
Yak! <yak1ex@users.noreply.github.com>
2022-08-20 16:43:55 +0900

    [node-bootstrap] Add workaround for canvas build.

--- node-bootstrap/pre-02-GTK_Root.js
+++ node-bootstrap/pre-02-GTK_Root.js
@@ -1,2 +1,6 @@
-const gtk_prefix = path.dirname(find_path('libgtk-win32-2.0-0.dll'));
-exec(`npm config set GTK_Root ${path.normalize(gtk_prefix)}`);
+const gtk_prefix = path.dirname(find_path('libgtk-win32-2.0-0.dll'))
+const gtk_root = path.normalize(gtk_prefix).replaceAll('\\','/')
+exec(`npm config set GTK_Root=${gtk_root}`)
+// I'm not sure the reason, but the above is not enough as of node-gyp@9.1.0
+process.env["npm_config_GTK_Root"]=gtk_root
+console.log(`env:npm_config_GTK_Root=${gtk_root}`)
