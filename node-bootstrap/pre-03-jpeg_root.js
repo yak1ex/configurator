547f38fef977c25f135b28bc0938ea1605bae581
Yak! <yak1ex@users.noreply.github.com>
2022-10-01 15:44:56 +0900

    [node-bootstrap] Add validness check for finding `jpeg62.dll' (refs GH-22, refs GH-46).

--- node-bootstrap/pre-03-jpeg_root.js
+++ node-bootstrap/pre-03-jpeg_root.js
@@ -1,2 +1,9 @@
-const libjpeg_turbo_prefix = path.dirname(find_path('jpeg62.dll'))
-exec(`npm config set jpeg_root=${path.normalize(libjpeg_turbo_prefix).replaceAll('\\','/')}`)
+/* global console, exec, find_path, path */
+const dll = 'jpeg62.dll'
+const dllPath = find_path(dll)
+if (dllPath === null) {
+  console.log(`${dll} is not found`)
+} else {
+  const libjpegTurboPrefix = path.dirname(dllPath)
+  exec(`npm config set jpeg_root=${path.normalize(libjpegTurboPrefix).replaceAll('\\', '/')}`)
+}
