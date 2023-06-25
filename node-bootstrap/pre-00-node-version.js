67fb08b1221561db6fff499bfc0bc79a1ce22c43
Yak! <yak1ex@users.noreply.github.com>
2023-06-25 16:38:31 +0900

    [node-bootstrap] Add version check for nvm use.

--- node-bootstrap/pre-00-node-version.js
+++ node-bootstrap/pre-00-node-version.js
@@ -1,1 +1,7 @@
-exec(`nvm use ${bootstrap.versions.node}`);
+node_version = exec_get('nvm current').trim()
+if (node_version === `v${bootstrap.versions.node}`) {
+    console.log(`skip nvm use because already ${node_version} is used`)
+} else {
+    exec(`nvm install ${bootstrap.versions.node}`);
+    exec(`nvm use ${bootstrap.versions.node}`);
+}
